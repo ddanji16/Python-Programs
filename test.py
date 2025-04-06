3f974d5b98d16f6375ae549feb971610baa24233
Daniel anthony Dela cruz <anthonydelac1602@gmail.com>
2025-04-06 17:02:54 +0800

    add two num

---
+++ test.py
@@ -0,0 +1,17 @@
+
+num1 = int(input("Enter First Number: "));
+
+num2 = int(input("Enter Second Number: "));
+
+
+sum = num1 + num2;
+
+
+print(" the sum of two numbers is:",sum);
+
+
+if sum <= 100:
+     print("less than 100")
+
+else:
+     print("above 100")
